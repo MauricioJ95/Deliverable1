6d02904a0139ef02c83cdc9b25d1d9a19af908c4
Mauricio Reyna <mauriciojreyna@gmail.com>
2018-12-07 10:38:21 -0500

    last is currency

--- Deliverable1/Program.cs
+++ Deliverable1/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,7 +11,7 @@ namespace Deliverable1
     {
         static void Main(string[] args)
         {
-            float num1 = 0, num2 = 0, num3 = 0;
+            float num1 = 0, num2 = 0, num3 = 0, total = 0;
             string input;
 
             Console.WriteLine("Please input a $ amount");
@@ -25,17 +26,36 @@ namespace Deliverable1
             input = Console.ReadLine();
             num3 = float.Parse(input);
 
-            List<float> amount = new List<float>();
-            amount.Add(num1);
-            amount.Add(num2);
-            amount.Add(num3);
-            amount.Max();
-            amount.Min();
-            amount.Average();
+            List<float> values = new List<float>();
+            values.Add(num1);
+            values.Add(num2);
+            values.Add(num3);
 
-            Console.WriteLine("Highest number is {0}", amount.Max());
-            Console.WriteLine("Lowest number is {0}", amount.Min());
-            Console.WriteLine("Average is {0}", amount.Average());
+            Console.WriteLine((num1 + num2 + num3));
+            input = Console.ReadLine();
+            total = float.Parse(input);
+
+            var usCulture = CultureInfo.GetCultureInfo("en-US");
+            var outputUS = string.Format("c", usCulture);
+
+            var svCulture = CultureInfo.GetCultureInfo("sv-Sweden");
+            var outputSV = string.Format("c", svCulture);
+
+            var jaCulture = CultureInfo.GetCultureInfo("ja-Japan");
+            var outputJA = string.Format("c", jaCulture);
+
+            var thCulture = CultureInfo.GetCultureInfo("th-Thai");
+            var outputTH = string.Format("c", thCulture);
+
+            Console.WriteLine("Highest number is {0}", values.Max());
+            Console.WriteLine("Lowest number is {0}", values.Min());
+            Console.WriteLine("Average is {0}", values.Average());
+
+
+            /*var usCulture = CultureInfo.GetCultureInfo("EN-US");
+
+            var output = string.Format("c", usCulture);*/
+
             Console.ReadLine();
 
         }
